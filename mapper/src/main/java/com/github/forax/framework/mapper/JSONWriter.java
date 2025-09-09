c749edda57457d8a87c2d50a75c937de4246600a
paul <paul.pensee@gmail.com>
2025-09-09 12:30:35 +0200

    q3

--- mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
+++ mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
@@ -1,7 +1,44 @@
 package com.github.forax.framework.mapper;
 
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public final class JSONWriter {
-  public String toJSON(Object o) {
-    throw new UnsupportedOperationException("TODO");
+
+    private record Property(String prefix, Method getter){}
+    private static final class PropertieDescriptorClassValue extends ClassValue<List<Property>>{
+        @Override
+        protected List<Property> computeValue(Class<?> type){
+            var beanInfo = Utils.beanInfo(type);
+            return Arrays.stream(beanInfo.getPropertyDescriptors()).filter(propertyDescriptor -> !propertyDescriptor.getName().equals(("class"))).map(property -> {
+                var prefix ='"' + property.getName() + "\": ";
+                var getter =property.getReadMethod();
+                return new Property(prefix,getter);
+            }).toList();
+        }
+    }
+
+    private static final PropertieDescriptorClassValue CLASS_VALUE= new PropertieDescriptorClassValue();
+
+    public String toJSON(Object o) {
+          return switch(o){
+              case null -> "null";
+              case Boolean b -> b.toString();
+              case String s -> "\""+ s + "\"";
+              case Integer i -> i.toString();
+              case Double d -> d.toString();
+              case Object o2 -> {
+                  var properties =CLASS_VALUE.get(o2.getClass());
+                  yield properties.stream()
+                          .map(property ->{
+                              var value = Utils.invokeMethod(o2,property.getter);
+                              return  property.prefix + toJSON(value);
+                          })
+                          .collect(Collectors.joining(", ","{","}"));
+              }
+          };
   }
 }
